cfca8c5afaed66a8d9683e1f2c077009eceaeb92
Kilian Reisinger <kilian.reisinger02@htl-salzburg.ac.at>
2021-05-11 22:24:55 +0200

    stack

---
+++ Assets/LetsCook/scripts/Stack.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stack : MonoBehaviour
+{
+    private GameObject IngredientStack;
+    List<string> ingredients = new List<string>(); 
+    void Start() {
+
+        IngredientStack = this.gameObject.transform.Find("ingredientStack").gameObject;
+    }
+    private void CheckIngredients() {
+  
+        foreach(Transform Ingredient in IngredientStack.transform) {
+            ingredients.Add(Ingredient.gameObject.transform.GetComponent<identifier>().ingredient);
+   
+        }
+    }
+
+    // void Update() {
+        // CheckIngredients();
+    // }
+}
---
+++ Assets/LetsCook/scripts/cook.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cook : MonoBehaviour
+{
+    public ParticleSystem smoke;
+    public int cookgrade; 
+    public bool cooked = false;
+    public bool burned = false;
+    public Material pattyCooked;
+    public Material pattyBurned;
+    private bool switchlock = false;
+    private bool switchlockBurn = false;
+    private bool stay = false;
+
+
+    void Start() {
+        smoke.Stop();
+    }
+
+    void Update() 
+    {
+        if(stay == true) {
+            Debug.Log("On Stove!");
+            if(cookgrade > 500 && switchlock == false) {
+                SetCooked();
+            }
+            if(cookgrade > 1000 &&  switchlockBurn == false) {
+                SetBurned();
+            }
+            cookgrade ++;
+        }
+    }
+
+    // private void OnTriggerStay(Collider other)  {
+    private void OnCollisionEnter(Collision collisionInfo)  {  
+
+ 
+        if(collisionInfo.gameObject.name == "stove") {
+            smoke.Play();
+            stay = true;
+        } 
+    }
+
+    private void OnCollisionExit(Collision collisionInfo)  {  
+        smoke.Stop();
+        stay = false;
+    }
+
+    private void SetCooked() {
+        switchlock = true;
+        cooked = true;
+        this.gameObject.GetComponent<MeshRenderer> ().material = pattyCooked;
+
+    }
+    private void SetBurned() {
+        switchlockBurn = true;
+        cooked = false;
+        burned = true;
+        this.gameObject.GetComponent<MeshRenderer> ().material = pattyBurned;
+
+    }
+    
+}
